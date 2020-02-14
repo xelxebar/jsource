d1e9bc1157461249096ca115e54f9bc7698a1124
B. Wilson <x@wilsonb.com>
2020-02-14 18:56:53 +0900

    make3:libj: Refactor logic into orthogonal units
    The bulk of `build_libj.sh` is dedicated to setting up the environment
    variables to be picked up by make. This commit performs a lot of clean
    up regarding the logic of how said variables are set.
    
    In particular, previously, much of the work was contained in
    deeply-nested `if` blocks and one large `case` statement. By making use
    of various forms of shell parameter expansion, we break up the
    monolithic blocks of logic into units that for the most part work on one
    variable at a time.
    
    This, hopefully, makes the intent clearer. As a bonus, the
    refactorization also decreases LOC by about half.

--- make3/build_libj.sh
+++ make3/build_libj.sh
@@ -1,112 +1,68 @@
 #!/usr/bin/env sh
 set -o errexit -o nounset -o noclobber
 
-if [ "$(uname -m)" = "armv6l" ] || [ "$(uname -m)" = "aarch64" ] || [ "${RASPI:-}" = 1 ]; then
-jplatform="${jplatform:=raspberry}"
-elif [ "$(uname)" = "Darwin" ]; then
-jplatform="${jplatform:=darwin}"
-else
-jplatform="${jplatform:=linux}"
-fi
-if [ "$(uname -m)" = "x86_64" ]; then
-j64x="${j64x:=j64avx}"
-elif [ "$(uname -m)" = "aarch64" ]; then
-j64x="${j64x:=j64}"
-else
-j64x="${j64x:=j32}"
-fi
+host=$(uname --machine)
 
+case "$host" in
+	armv6l|aarch64) jplatform=${jplatform:-raspberry};;
+	Darwin) jplatform=${jplatform:-darwin};;
+	*) jplatform=${jplatform:-linux};;
+esac
+[ "${RASPI:-}" ] && jplatform=${jplatform:-raspberry};
+
+case "$host" in
+	x86_64) j64x=${j64x:-j64avx};;
+	aarch64) j64x=${j64x:-j64};;
+	*) j64x=${j64x:-j32};;
+esac
+
+[ -f /usr/bin/cc ] && CC=${CC:-cc}
+[ -f /usr/bin/clang ] && CC=${CC:-clang}
+CC=${CC:-gcc}
+
+macmin="-mmacosx-version-min=10.6"
+# compiler=$("$CC" --version | head -n 1)
+compiler=$(readlink -f "$(command -v "$CC")" 2> /dev/null || echo "$CC")
+[ -z "${compiler##*gcc*}" ] || [ -z "${CC##*gcc*}" ] && gcc=1
+
+[ "${USE_OPENMP:-}" ] && LDOPENMP32="-l:libomp.so.5" # clang
+[ "${USE_OPENMP:-}" ] && [ "${gcc:-}" ] && LDOPENMP32="-l:libgomp.so.1"
+[ "${USE_OPENMP:-}" ] && OPENMP="-fopenmp" && LDOPENMP="-fopenmp"
+
+[ "${USE_THREAD:-}" ] && USETHREAD="=DUSE_THREAD"
+[ "${USE_THREAD:-}" ] && LDTHREAD='-pthread'
+[ "${VERBOSELOG:-}" ] && FVERBOSELOG="-DVERBOSELOG"
+
+CFLAGS="${CFLAGS:-} ${OPENMP:-} ${USETHREAD:-} ${FVERBOSELOG:-} -Werror -fPIC -O2 -fvisibility=hidden -fwrapv -fno-strict-aliasing -Wextra"
 # gcc 5 vs 4 - killing off linux asm routines (overflow detection)
 # new fast code uses builtins not available in gcc 4
 # use -DC_NOMULTINTRINSIC to continue to use more standard c in version 4
 # too early to move main linux release package to gcc 5
-
-macmin="-mmacosx-version-min=10.6"
-
-if [ "x${CC:-}" = x'' ] ; then
-if [ -f "/usr/bin/cc" ]; then
-CC=cc
-else
-if [ -f "/usr/bin/clang" ]; then
-CC=clang
-else
-CC=gcc
-fi
-fi
-export CC
-fi
-# compiler=$("$CC" --version | head -n 1)
-compiler=$(readlink -f "$(command -v "$CC")" 2> /dev/null || echo "$CC")
-echo "CC=$CC"
-echo "compiler=$compiler"
-
-USE_OPENMP="${USE_OPENMP:=0}"
-if [ "$USE_OPENMP" -eq 1 ] ; then
-OPENMP=" -fopenmp "
-LDOPENMP=" -fopenmp "
-if [ -z "${compiler##*gcc*}" ] || [ -z "${CC##*gcc*}" ]; then
-LDOPENMP32=" -l:libgomp.so.1 "    # gcc
-else
-LDOPENMP32=" -l:libomp.so.5 "     # clang
-fi
-fi
-
-USE_THREAD="${USE_THREAD:=0}"
-if [ "$USE_THREAD" -eq 1 ] ; then
-USETHREAD=" -DUSE_THREAD "
-LDTHREAD=" -pthread "
-fi
-
-VERBOSELOG="${VERBOSELOG:=0}"
-if [ "$VERBOSELOG" -eq 1 ] ; then
-FVERBOSELOG=" -DVERBOSELOG "
-fi
-
-if [ -z "${compiler##*gcc*}" ] || [ -z "${CC##*gcc*}" ]; then
-# gcc
-common="${OPENMP:-} ${USETHREAD:-} ${FVERBOSELOG:-} -Werror -fPIC -O2 -fvisibility=hidden -fwrapv -fno-strict-aliasing -Wextra -Wno-unused-parameter -Wno-sign-compare -Wno-clobbered -Wno-empty-body -Wno-unused-value -Wno-pointer-sign -Wno-parentheses -Wno-type-limits"
-GNUC_MAJOR=$(echo __GNUC__ | "$CC" -E -x c - | tail -n 1)
-# shellcheck disable=SC2034
-GNUC_MINOR=$(echo __GNUC_MINOR__ | "$CC" -E -x c - | tail -n 1)
-if [ "$GNUC_MAJOR" -ge 5 ] ; then
-common="$common -Wno-maybe-uninitialized"
-else
-common="$common -DC_NOMULTINTRINSIC -Wno-uninitialized"
-fi
-if [ "$GNUC_MAJOR" -ge 6 ] ; then
-common="$common -Wno-shift-negative-value"
-fi
-# alternatively, add comment /* fall through */
-if [ "$GNUC_MAJOR" -ge 7 ] ; then
-common="$common -Wno-implicit-fallthrough"
-fi
-if [ "$GNUC_MAJOR" -ge 8 ] ; then
-common="$common -Wno-cast-function-type"
-fi
-else
-# clang 3.4
-common="${OPENMP:-} ${USETHREAD:-} ${FVERBOSELOG:-} -Werror -fPIC -O2 -fvisibility=hidden -fwrapv -fno-strict-aliasing -Wextra -Wno-consumed -Wuninitialized -Wno-unused-parameter -Wsign-compare -Wno-empty-body -Wno-unused-value -Wno-pointer-sign -Wno-parentheses -Wunsequenced -Wno-string-plus-int -Wtautological-constant-out-of-range-compare"
-# clang 3.8
-CLANG_MAJOR=$(echo __clang_major__ | "$CC" -E -x c - | tail -n 1)
-CLANG_MINOR=$(echo __clang_minor__ | "$CC" -E -x c - | tail -n 1)
-if [ "$CLANG_MAJOR" -eq 3 ] && [ "$CLANG_MINOR" -ge 8 ] ; then
-common="$common -Wno-pass-failed"
-else
-if [ "$CLANG_MAJOR" -ge 4 ] ; then
-common="$common -Wno-pass-failed"
-fi
-fi
-# clang 10
-if [ "$CLANG_MAJOR" -ge 10 ] ; then
-common="$common -Wno-implicit-float-conversion"
-fi
+if [ "${gcc:-}" ]; then
+	CFLAGS="${CFLAGS:-} -Wno-unused-parameter -Wno-sign-compare -Wno-clobbered -Wno-empty-body -Wno-unused-value -Wno-pointer-sign -Wno-parentheses -Wno-type-limits"
+	GNUC_MAJOR=$(echo __GNUC__ | "$CC" -E -x c - | tail -n 1)
+	# shellcheck disable=SC2034
+	GNUC_MINOR=$(echo __GNUC_MINOR__ | "$CC" -E -x c - | tail -n 1)
+	[ "$GNUC_MAJOR" -lt 5 ] && CFLAGS="${CFLAGS:-} -DC_NOMULTINTRINSIC -Wno-uninitialized"
+	[ "$GNUC_MAJOR" -ge 5 ] && CFLAGS="${CFLAGS:-} -Wno-maybe-uninitialized"
+	[ "$GNUC_MAJOR" -ge 6 ] && CFLAGS="${CFLAGS:-} -Wno-shift-negative-value"
+	# alternatively, add comment /* fall through */
+	[ "$GNUC_MAJOR" -ge 7 ] && CFLAGS="${CFLAGS:-} -Wno-implicit-fallthrough"
+	[ "$GNUC_MAJOR" -ge 8 ] && CFLAGS="${CFLAGS:-} -Wno-cast-function-type"
+else # clang 3.4
+	CFLAGS="${CFLAGS:-} -Wno-consumed -Wuninitialized -Wno-unused-parameter -Wsign-compare -Wno-empty-body -Wno-unused-value -Wno-pointer-sign -Wno-parentheses -Wunsequenced -Wno-string-plus-int -Wtautological-constant-out-of-range-compare"
+	CLANG_MAJOR=$(echo __clang_major__ | "$CC" -E -x c - | tail -n 1)
+	CLANG_MINOR=$(echo __clang_minor__ | "$CC" -E -x c - | tail -n 1)
+	[ "$CLANG_MAJOR" -eq 3 ] && [ "$CLANG_MINOR" -ge 8 ] && CFLAGS="${CFLAGS:-} -Wno-pass-failed"
+	[ "$CLANG_MAJOR" -ge 4 ] && CFLAGS="${CFLAGS:-} -Wno-pass-failed"
+	[ "$CLANG_MAJOR" -ge 10 ] && CFLAGS="${CFLAGS:-} -Wno-implicit-float-conversion"
 fi
 
 NO_SHA_ASM="${NO_SHA_ASM:=0}"
 
 if [ "$NO_SHA_ASM" -ne 0 ] ; then
 
-common="$common -DNO_SHA_ASM"
+CFLAGS="${CFLAGS:-} -DNO_SHA_ASM"
 
 else
 
@@ -160,208 +116,83 @@ OBJS_ASM_WIN32=" \
 
 fi
 
+case "$j64x" in
+	j64avx2) j64=1; javx=1; javx2=1;;
+	j64avx) j64=1; javx=1;;
+	j64) j64=1;;
+	j32) j32=1;;
+esac
+
+case "$jplatform" in
+	linux|raspberry) TARGET="libj.so";;
+	darwin) TARGET="libj.dylib";;
+	windows) TARGET="j.dll";;
+esac
+
+case "$jplatform" in
+	linux) LDFLAGS="${LDFLAGS:-} ${j32:+-m32} -shared -Wl,-soname,libj.so -lm -ldl ${j32:+$LDOPENMP32} ${j64:+$LDOPENMP} ${LDTHREAD:-}";;
+	raspberry) LDFLAGS="${LDFLAGS:-} -shared -Wl,-soname,libj.so -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-}";;
+	darwin) LDFLAGS="${LDFLAGS:-} ${j32:+-m32} -dynamiclib -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-} $macmin";;
+	windows) LDFLAGS="${LDFLAGS:-} ${j32:+$LDOPENMP32} ${j64:-$LDOPENMP} -shared -Wl,--enable-stdcall-fixup -lm -static-libgcc -static-libstdc++ ${LDTHREAD:-}";;
+esac
+
+case "$jplatform" in
+	# faster, but sse2 not available for 32-bit amd cpu
+	# sse does not support mfpmath=sse in 32-bit gcc
+	# slower, use 387 fpu and truncate extra precision
+	# CFLAGS="${CFLAGS:-} -m32 -ffloat-store "
+	linux) CFLAGS="${CFLAGS:-} ${j32:+-m32 -msse2 -mfpmath=sse} ${javx:+-DC_AVX=1} ${javx2:+-DC_AVX2=1}";;
+	raspberry) CFLAGS="${CFLAGS:-} ${j32:+-marm -march=armv6 -mfloat-abi=hard -mfpu=vfp -DRASPI} ${j64:+-march=armv8-a+crc -DRASPI -DC_CRC32C=1}";;
+	darwin) CFLAGS="${CFLAGS:-} $macmin ${j32:+-m32} ${javx:+-DC_AVX=1} ${javx2:+-DC_AVX2=1}";;
+	windows) CFLAGS="${CFLAGS:-} ${DOLECOM:-} ${j32:+-m32 -msse2 -mfpmath=sse} -D_FILE_OFFSET_BITS=64 -D_JDLL ${javx:+-DC_AVX=1} ${javx2:+-DC_AVX2=1}"
+esac
+
+case "$jplatform" in
+	linux|darwin|windows) OBJS_AESNI="aes-ni.o";;
+	raspberry);;
+esac
+
+case "$jplatform" in
+	darwin) GASM_FLAGS="${j32:+-m32} $macmin";;
+	linux) GASM_FLAGS="${j32:+-m32}";;
+	raspberry|widows);;
+esac
+
+case "$jplatform" in
+	linux) SRC_ASM="${j32:+$SRC_ASM_LINUX32}${j64:+$SRC_ASM_LINUX}";;
+	raspberry) SRC_ASM="${j32:+$SRC_ASM_RASPI32}${j64:+$SRC_ASM_RASPI}";;
+	darwin) SRC_ASM="${j32:+$SRC_ASM_MAC32}${j64:+$SRC_ASM_MAC}";;
+	windows) SRC_ASM="${j32:+$SRC_ASM_WIN32}${j64:+$SRC_ASM_WIN}";;
+esac
+
+case "$jplatform" in
+	raspberry) OBJS_AESARM="${j64:+aes-arm.o}";;
+	linux|darwin|windows);;
+esac
+
+CFLAGS_SIMD="${javx:+-mavx} ${javx2:+-mavx2 -mfma}"
+OBJS_FMA="${javx:+gemm_int-fma.o}"
+
+# faster, but sse2 not available for 32-bit amd cpu
+# sse does not support mfpmath=sse in 32-bit gcc
+# slower, use 387 fpu and truncate extra precision
+# CFLAGS="${CFLAGS:-} -m32 -ffloat-store "
+if [ "$jplatform" = "windows" ]; then
+	DLLOBJS="jdll.o"
+	LIBJDEF="../../../../dllsrc/jdll2.def"
+	[ "${jolecom:-}" ] && DLLOBJS="$DLLOBJS jdllcomx.o"
+	[ "${jolecom:-}" ] && LIBJDEF="../../../../dllsrc/jdll.def"
+	[ "${jolecom:-}" ] && DOLECOM="-DOLECOM"
+	LIBJRES="jddlres.o"
+	OBJS_ASM="${j32:+$OBJS_ASM_WIN32}${j64:+$OBJS_ASM_WIN}"
+fi
+
 case "${jplatform}_$j64x" in
-
-linux_j32) # linux x86
-TARGET=libj.so
-# faster, but sse2 not available for 32-bit amd cpu
-# sse does not support mfpmath=sse in 32-bit gcc
-CFLAGS="$common -m32 -msse2 -mfpmath=sse "
-# slower, use 387 fpu and truncate extra precision
-# CFLAGS="$common -m32 -ffloat-store "
-LDFLAGS=" -shared -Wl,-soname,libj.so -m32 -lm -ldl ${LDOPENMP32:-} ${LDTHREAD:-}"
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_LINUX32}"
-GASM_FLAGS="-m32"
-;;
-
-linux_j64) # linux intel 64bit nonavx
-TARGET=libj.so
-CFLAGS="$common "
-LDFLAGS=" -shared -Wl,-soname,libj.so -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-}"
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_LINUX}"
-GASM_FLAGS=""
-;;
-
-linux_j64avx) # linux intel 64bit avx
-TARGET=libj.so
-CFLAGS="$common -DC_AVX=1 "
-LDFLAGS=" -shared -Wl,-soname,libj.so -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-}"
-CFLAGS_SIMD=" -mavx "
-OBJS_FMA=" gemm_int-fma.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_LINUX}"
-GASM_FLAGS=""
-;;
-
-linux_j64avx2) # linux intel 64bit avx2
-TARGET=libj.so
-CFLAGS="$common -DC_AVX=1 -DC_AVX2=1 "
-LDFLAGS=" -shared -Wl,-soname,libj.so -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-}"
-CFLAGS_SIMD=" -mavx2 -mfma "
-OBJS_FMA=" gemm_int-fma.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_LINUX}"
-GASM_FLAGS=""
-;;
-
-raspberry_j32) # linux raspbian arm
-TARGET=libj.so
-CFLAGS="$common -marm -march=armv6 -mfloat-abi=hard -mfpu=vfp -DRASPI "
-LDFLAGS=" -shared -Wl,-soname,libj.so -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-}"
-SRC_ASM="${SRC_ASM_RASPI32}"
-GASM_FLAGS=""
-;;
-
-raspberry_j64) # linux arm64
-TARGET=libj.so
-CFLAGS="$common -march=armv8-a+crc -DRASPI -DC_CRC32C=1 "
-LDFLAGS=" -shared -Wl,-soname,libj.so -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-}"
-OBJS_AESARM=" aes-arm.o "
-SRC_ASM="${SRC_ASM_RASPI}"
-GASM_FLAGS=""
-;;
-
-darwin_j32) # darwin x86
-TARGET=libj.dylib
-CFLAGS="$common -m32 $macmin"
-LDFLAGS=" -dynamiclib -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-} -m32 $macmin"
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_MAC32}"
-GASM_FLAGS="-m32 $macmin"
-;;
-
-darwin_j64) # darwin intel 64bit nonavx
-TARGET=libj.dylib
-CFLAGS="$common $macmin"
-LDFLAGS=" -dynamiclib -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-} $macmin"
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_MAC}"
-GASM_FLAGS="$macmin"
-;;
-
-darwin_j64avx) # darwin intel 64bit
-TARGET=libj.dylib
-CFLAGS="$common $macmin -DC_AVX=1 "
-LDFLAGS=" -dynamiclib -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-} $macmin"
-CFLAGS_SIMD=" -mavx "
-OBJS_FMA=" gemm_int-fma.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_MAC}"
-GASM_FLAGS="$macmin"
-;;
-
-darwin_j64avx2) # darwin intel 64bit
-TARGET=libj.dylib
-CFLAGS="$common $macmin -DC_AVX=1 -DC_AVX2=1 "
-LDFLAGS=" -dynamiclib -lm -ldl ${LDOPENMP:-} ${LDTHREAD:-} $macmin"
-CFLAGS_SIMD=" -mavx2 -mfma "
-OBJS_FMA=" gemm_int-fma.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_MAC}"
-GASM_FLAGS="$macmin"
-;;
-
-windows_j32) # windows x86
-jolecom="${jolecom:=0}"
-if [ $jolecom -eq 1 ] ; then
-DOLECOM="-DOLECOM"
-fi
-TARGET=j.dll
-# faster, but sse2 not available for 32-bit amd cpu
-# sse does not support mfpmath=sse in 32-bit gcc
-CFLAGS="$common ${DOLECOM:-} -m32 -msse2 -mfpmath=sse -D_FILE_OFFSET_BITS=64 -D_JDLL "
-# slower, use 387 fpu and truncate extra precision
-# CFLAGS="$common -m32 -ffloat-store "
-LDFLAGS=" -shared -Wl,--enable-stdcall-fixup -lm -static-libgcc -static-libstdc++ ${LDOPENMP32:-} ${LDTHREAD:-}"
-if [ "$jolecom" -eq 1 ] ; then
-DLLOBJS=" jdll.o jdllcomx.o "
-LIBJDEF=" ../../../../dllsrc/jdll.def "
-else
-DLLOBJS=" jdll.o "
-LIBJDEF=" ../../../../dllsrc/jdll2.def "
-fi
-LIBJRES=" jdllres.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_WIN32}"
-OBJS_ASM="${OBJS_ASM_WIN32}"
-GASM_FLAGS=""
-;;
-
-windows_j64) # windows intel 64bit nonavx
-jolecom="${jolecom:=0}"
-if [ "$jolecom" -eq 1 ] ; then
-DOLECOM="-DOLECOM"
-fi
-TARGET=j.dll
-CFLAGS="$common ${DOLECOM:-} -D_FILE_OFFSET_BITS=64 -D_JDLL "
-LDFLAGS=" -shared -Wl,--enable-stdcall-fixup -lm -static-libgcc -static-libstdc++ ${LDOPENMP:-} ${LDTHREAD:-}"
-if [ "$jolecom" -eq 1 ] ; then
-DLLOBJS=" jdll.o jdllcomx.o "
-LIBJDEF=" ../../../../dllsrc/jdll.def "
-else
-DLLOBJS=" jdll.o "
-LIBJDEF=" ../../../../dllsrc/jdll2.def "
-fi
-LIBJRES=" jdllres.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_WIN}"
-OBJS_ASM="${OBJS_ASM_WIN}"
-GASM_FLAGS=""
-;;
-
-windows_j64avx) # windows intel 64bit avx
-jolecom="${jolecom:=0}"
-if [ "$jolecom" -eq 1 ] ; then
-DOLECOM="-DOLECOM"
-fi
-TARGET=j.dll
-CFLAGS="$common ${DOLECOM:-} -DC_AVX=1 -D_FILE_OFFSET_BITS=64 -D_JDLL "
-LDFLAGS=" -shared -Wl,--enable-stdcall-fixup -lm -static-libgcc -static-libstdc++ ${LDOPENMP:-} ${LDTHREAD:-}"
-CFLAGS_SIMD=" -mavx "
-if [ "$jolecom" -eq 1 ] ; then
-DLLOBJS=" jdll.o jdllcomx.o "
-LIBJDEF=" ../../../../dllsrc/jdll.def "
-else
-DLLOBJS=" jdll.o "
-LIBJDEF=" ../../../../dllsrc/jdll2.def "
-fi
-LIBJRES=" jdllres.o "
-OBJS_FMA=" gemm_int-fma.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_WIN}"
-OBJS_ASM="${OBJS_ASM_WIN}"
-GASM_FLAGS=""
-;;
-
-windows_j64avx2) # windows intel 64bit avx
-jolecom="${jolecom:=0}"
-if [ "$jolecom" -eq 1 ] ; then
-DOLECOM="-DOLECOM"
-fi
-TARGET=j.dll
-CFLAGS="$common ${DOLECOM:-} -DC_AVX=1 -DC_AVX2=1 -D_FILE_OFFSET_BITS=64 -D_JDLL "
-LDFLAGS=" -shared -Wl,--enable-stdcall-fixup -lm -static-libgcc -static-libstdc++ ${LDOPENMP:-} ${LDTHREAD:-}"
-CFLAGS_SIMD=" -mavx2 -mfma "
-if [ "$jolecom" -eq 1 ] ; then
-DLLOBJS=" jdll.o jdllcomx.o "
-LIBJDEF=" ../../../../dllsrc/jdll.def "
-else
-DLLOBJS=" jdll.o "
-LIBJDEF=" ../../../../dllsrc/jdll2.def "
-fi
-LIBJRES=" jdllres.o "
-OBJS_FMA=" gemm_int-fma.o "
-OBJS_AESNI=" aes-ni.o "
-SRC_ASM="${SRC_ASM_WIN}"
-OBJS_ASM="${OBJS_ASM_WIN}"
-GASM_FLAGS=""
-;;
-
-*)
-echo no case for those parameters
-exit 1
+	linux_j32|linux_j64|linux_j64avx|linux_j64avx2);;
+	raspberry_j32|raspberry_j64);;
+	darwin_j32|darwin_j64|darwin_j64avx|darwin_j64avx2);;
+	windows_j32|windows_j64|windows_j64avx|windows_j64avx2);;
+	*) >&2 echo "ERROR: Incompatible parameter set: $jplatform $j64x"; exit 1;;
 esac
 
 cat <<-VARS
@@ -382,16 +213,13 @@ jplatform=${jplatform:-}
 j64x=${j64x:-}
 VARS
 
-if [ ! -f ../jsrc/jversion.h ] ; then
-  cp ../jsrc/jversion-x.h ../jsrc/jversion.h
-fi
-
 jmake=$(dirname "$0")
 jsource=$jmake/..
 
+[ -r "$jsource/jsrc/jversion.h" ] || cp "$jsource/jsrc/jversion-x.h" "$jsource/jsrc/jversion.h"
 mkdir -p "$jsource/bin/$jplatform/$j64x"
 mkdir -p "$jmake/obj/$jplatform/$j64x/"
 cp makefile-libj "obj/$jplatform/$j64x/."
 
-export CFLAGS LDFLAGS TARGET CFLAGS_SIMD GASM_FLAGS DLLOBJS LIBJDEF LIBJRES OBJS_FMA OBJS_AESNI OBJS_AESARM OBJS_ASM SRC_ASM jplatform j64x
+export CC CFLAGS LDFLAGS TARGET CFLAGS_SIMD GASM_FLAGS DLLOBJS LIBJDEF LIBJRES OBJS_FMA OBJS_AESNI OBJS_AESARM OBJS_ASM SRC_ASM jplatform j64x
 make -C "$jmake/obj/$jplatform/$j64x/" -f makefile-libj
